a805e7627a754b43fc2339f9a99840de70edeb82
Ara Garrido <94240265+invitacionesweb@users.noreply.github.com>
2023-12-22 11:19:40 -0300

    ACTUALIZACION
    AC

--- countdown.js
+++ countdown.js
@@ -30,4 +30,4 @@ const countdown = (deadline, elem, finalMessage ) => {
 };
 
 
-countdown('SEP 24 2023 10:43:51 GMT-0300', 'clock', 'llego el dia')
+countdown('nov 05 2024 10:43:51 GMT-0300', 'clock', 'llego el dia')
